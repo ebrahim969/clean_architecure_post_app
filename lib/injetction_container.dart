6b1164f94026094fcd86b08f10059dfd8a34c4a8
ebrahim969 <ebrahimelnemr084@gmail.com>
2024-03-17 19:04:05 -0700

    finish dependency injection

---
+++ lib/injetction_container.dart
@@ -0,0 +1,51 @@
+import 'package:clean_architicure_posts/core/network/network_info.dart';
+import 'package:clean_architicure_posts/features/posts/data/data_source/posts_local_data_source.dart';
+import 'package:clean_architicure_posts/features/posts/data/data_source/posts_remote_data_source.dart';
+import 'package:clean_architicure_posts/features/posts/data/repos/post_repo_impl.dart';
+import 'package:clean_architicure_posts/features/posts/domain/repos/posts_repo.dart';
+import 'package:clean_architicure_posts/features/posts/domain/use_cases/add_post_usecase.dart';
+import 'package:clean_architicure_posts/features/posts/domain/use_cases/delete_post_usecase.dart';
+import 'package:clean_architicure_posts/features/posts/domain/use_cases/get_posts_usecase.dart';
+import 'package:clean_architicure_posts/features/posts/domain/use_cases/update_post_usecase.dart';
+import 'package:clean_architicure_posts/features/posts/presentation/bloc/add_update_delete_post/add_update_delete_post_bloc.dart';
+import 'package:clean_architicure_posts/features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features =posts
+
+  //Bloc
+  sl.registerFactory(() => PostsBloc(getAllPosts: sl()));
+  sl.registerFactory(() => AddUpdateDeletePostBloc(
+      addPost: sl(), updatePost: sl(), deletePost: sl()));
+  //UseCases
+  sl.registerLazySingleton(() => GetAllPostsUseCase(sl()));
+  sl.registerLazySingleton(() => AddPostUseCase(sl()));
+  sl.registerLazySingleton(() => UpdatePostUseCase(sl()));
+  sl.registerLazySingleton(() => DeletePostUseCase(sl()));
+  //Repository
+  sl.registerLazySingleton<PostsRepo>(() => PostRepoImpl(
+        remoteDataSource: sl(),
+        localDataSource: sl(),
+        networkInfo: sl(),
+      ));
+  //Data Sources
+  sl.registerLazySingleton<PostRemoteDataSource>(
+    () => PostRemoteDataSourceImplement(client: sl()),
+  );
+  sl.registerLazySingleton<PostsLocalDataSource>(
+    () => PostsLocalDataSourceImplement(sharedPreferences: sl()),
+  );
+  //! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+  //! External
+  final sharedPreferences = SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => sharedPreferences);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:clean_architicure_posts/features/posts/presentation/bloc/add_update_delete_post/add_update_delete_post_bloc.dart';
+import 'package:clean_architicure_posts/features/posts/presentation/bloc/posts/posts_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'injetction_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const PostsApp());
 }
 
@@ -9,10 +15,16 @@ class PostsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello"),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => di.sl<PostsBloc>()),
+        BlocProvider(create: (_) => di.sl<AddUpdateDeletePostBloc>())
+      ],
+      child: const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text("Hello"),
+          ),
         ),
       ),
     );
